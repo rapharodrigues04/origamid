c77fb12da28b7af9951af49f67ff744e97585fd3
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2021-01-22 21:06:56 -0300

    js modulo 01

---
+++ javascript_ES6/01-javascript-para-iniciantes/01-variaveis/1.variveis.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+let nome = 'Raphael';
+
+// Declarar uma variável com a sua idade
+let idade = 35;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Massas';
+console.log(comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+let hobbie, 
+    filme, 
+    time, 
+    filho, 
+    idadeFilho;
